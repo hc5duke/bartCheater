8929c1d50cee232eefa169b85b0e60408b067717
hc5duke <hc5duke@gmail.com>
2009-12-12 23:13:18 -0800

    mouseovers for trains

--- public/main.js
+++ public/main.js
@@ -1,18 +1,16 @@
 document.observe('dom:loaded', function(){
-  $('refresh').onclick = function() {
-    location.reload(true);
-  };
+  var refresh = $('refresh');
+  refresh.onclick = function() { location.reload(true); };
+
   var color = $('color');
-  if (color) {
-    color.onclick = function() {
-      location.search = "?color=true";
-    }
-  }
+  if (color) { color.onclick = function() { location.search = "?color=true"; } }
 
   var noColor = $('no_color');
-  if (noColor) {
-    noColor.onclick = function() {
-      location.search = "";
+  if (noColor) { noColor.onclick = function() { location.search = ""; } }
+
+  $('home').select('div.train').each(function(train){
+    train.onclick = function() {
+      console.log('click !')
     }
-  }
+  });
 });
