e87ac82146907136687e23749cc909051a539769
hc5duke <hc5duke@gmail.com>
2009-12-12 22:41:17 -0800

    ehh just use prototype

--- public/main.js
+++ public/main.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-  $('#refresh').click(function() {
+document.observe('dom:loaded', function(){
+  $('refresh').onclick = function() {
     location.reload(true);
-  });
-})
+  };
+  var color = $('color');
+  if (color) {
+    color.onclick = function() {
+      location.search = "?color=true";
+    }
+  }
+
+  var noColor = $('no_color');
+  if (noColor) {
+    noColor.onclick = function() {
+      location.search = "";
+    }
+  }
+});
